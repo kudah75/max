8b8840bc9235cec1c6230adf3396a1782c4cb75f
lemur <polanster@gmail.com>
2020-04-15 09:07:16 +0200

    first commit

---
+++ lpf-content/pages/home/index.php
@@ -0,0 +1,41 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); 
+/* ===
+
+TITLE: Мой будущий лендинг (быть может)
+
+META:
+	description: Landing Page Framework
+	keywords: Landing page, php-framework
+	viewport: width=device-width, initial-scale=1.0
+	generator: Landing Page Framework (lpf.maxsite.com.ua)
+
+VAR:
+	simple: true
+	compress_text: true
+        nocss: true
+=== */
+
+?>
+
+h1 Мой будущий лендинг
+
+<div class="layout-center-wrap bg-blue800 t-white " style="padding: 50px 0 100px 0;">
+<div class="layout-wrap">
+<div class="flex flex-vcenter">
+<div class="flex flex-vcenter w20">
+<div class="i-gg-circle t-red t250"></div>
+<div class="w100 bold t-white">Sturtup</div>
+</div>
+<div class="w80 t-right">
+<a class="t-white hover-t-blue mar25-r" href="#">Home</a>
+<a class="t-white hover-t-blue mar25-r" href="#">Work</a>
+<a class="t-white hover-t-blue mar25-r" href="#">Blog</a>
+<a class="t-white hover-t-blue" href="#">Contact</a>
+</div>
+</div>
+<div class="mar50-t t150 t-center">We have created the product that will help <br>your startup to look even better</div>
+<div class="mar20-t t-center">
+<a class="mar20-t mar5-r button bg-blue700 hover-bg-blue500 t90" href="#">Learn more</a>
+<a class="mar20-t button bg-red700 hover-bg-red500 t90 t-center" href="#">Buy now for 9$</a>
+</div>
+</div></div>
